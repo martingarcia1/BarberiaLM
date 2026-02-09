a1cc1322d79b6da99c49437b0e6d1f24bf355b89
Martin <martigarcia222@gmail.com>
2026-02-09 13:04:54 -0300

    fix: correcion api/admin

--- Front-End/src/utils/url.js
+++ Front-End/src/utils/url.js
@@ -1,5 +1,8 @@
 import { API_URL } from '../config/api';
 
+// Si API_URL termina en /api, lo quitamos para evitar duplicados en componentes legacy que añaden /api manualmente
+const baseUrl = API_URL.endsWith('/api') ? API_URL.slice(0, -4) : API_URL;
+
 export default {
-    urlKey: API_URL
+    urlKey: baseUrl
 }
